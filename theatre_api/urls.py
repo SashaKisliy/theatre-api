79819b8ea0fe57d720b8dc9cc20fe6b18d89cb53
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-12 23:09:33 +0300

    add api/theatre endpoint

--- theatre_api/urls.py
+++ theatre_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/", include("user.urls"), name="user"),
+    path("api/theatre/", include("theatre.urls"), name="theatre"),
 ]
